758870fbad769ca0fc7d9f75d16f443ca50ec495
Thomas Schmitt <schmitt@lmz-bw.de>
2011-06-07 10:13:34 +0000

    * update-linbofs:    * Neustart der Downloaddienste Multicast und Bittorrent am Ende des Skripts ergänzt (betr. #463).    * Locking-Mechanismus implementiert, damit Skript nicht mehrfach gestartet werden kann.  * helperfunctions.sh: Ermittlung aktiver Images verbessert, image_in_use() nach active_images() umbenannt.  * linbo-multicast:    * Verbesserungen bzgl. helperfunctions.sh (s.o.),    * Logging gefixt (schließt #118).  * linbo-bittorrent: Verbesserungen bzgl. helperfunctions.sh (s.o.).  * logrotate: Create-Befehl ergänzt.  * default: LOGFILE-Variable entfernt.  * rules: cloop-Downloadadresse aktualisiert.  * Kernel: aktualisiert auf 2.6.39.1.

--- share/helperfunctions.sh
+++ share/helperfunctions.sh
@@ -1,27 +1,55 @@
+# $Id$
 
-# check if image is in use
-image_in_use() {
- local image="$(basename $1)"
- local configs="$(grep -H $image $LINBODIR/start.conf.* | grep -v \:# | awk -F\: '{ print $1 }')"
- # check if image is defined in start.conf
- [ -z "$configs" ] && return 1
+# return active images
+active_images() {
+ # check for workstation data
+ [ -z "$WIMPORTDATA" ] && return 1
+ [ -s "$WIMPORTDATA" ] || return 1
+ # get active groups
+ local actgroups="$(grep ^[-_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789] $WIMPORTDATA | awk -F\; '{ print $3 }' | sort -u)"
+ [ -z "$actgroups" ] && return 0
+ # compute images used by active groups
+ local tmpfile=/var/tmp/active_images.$$
+ rm -f $tmpfile
+ touch $tmpfile || return 1
  local i=""
- local g=""
- local ret=""
- for i in $configs; do
-  # check if any group is defined in workstations
-  g="$(basename $i)"
-  g="${g#start.conf.}"
-  ret="$(grep \;$g\; $WIMPORTDATA | grep -v ^# | awk -F\; '{ print $3 }')"
-  [ -n "$ret" ] && return 0
+ for i in $actgroups; do
+  if [ -s "$LINBODIR/start.conf.$i" ]; then
+   grep -i ^baseimage $LINBODIR/start.conf.$i | awk -F\= '{ print $2 }' | awk '{ print $1 }' >> $tmpfile
+   grep -i ^image $LINBODIR/start.conf.$i | awk -F\= '{ print $2 }' | awk '{ print $1 }' >> $tmpfile
+  fi
  done
- return 1
+ local actimages="$(sort -u $tmpfile)"
+ rm $tmpfile
+ for i in $actimages; do
+  [ -s "$LINBODIR/$i" ] && echo "$i"
+ done
+ return 0
+}
+
+# check torrent against image
+check_torrent() {
+ local image="$1"
+ local torrent="$image.torrent"
+ cd "$LINBODIR"
+ [ -s "$image" ] || return 1
+ [ -s "$torrent" ] || return 1
+ local tmpfile=/var/tmp/check_torrent.$$
+ btshowmetainfo "$torrent" > $tmpfile || return 1
+ local filename="$(grep ^"file name" $tmpfile | awk '{ print $3 }')"
+ local filesize="$(grep ^"file size" $tmpfile | awk '{ print $3 }')"
+ rm $tmpfile
+ [ "$filename" = "$image" ] || return 1
+ local imagesize="$(ls -l $image | awk '{ print $5 }')"
+ [ "$filesize" = "$imagesize" ] || return 1
+ return 0
 }
 
 # create torrent file for image
 create_torrent() {
  local image="$1"
  local RC=1
+ cd "$LINBODIR"
  [ -s "$image" ] || return "$RC"
  local serverip="$2"
  local port="$3"
