56f4b0cb8af9af2c709cfe0a8f2be833500dda2c
Frank Schütte <fschuett@gymnasium-himmelsthuer.de>
2015-04-23 18:51:45 +0200

    Detect not only sd? disks but mmc block devices also.

--- linbo/linbo_cmd.sh
+++ linbo/linbo_cmd.sh
@@ -288,6 +288,38 @@ kernelfstype(){
  echo "$kernelfstype"
 }
 
+# fschuett
+# extract block device name for sd?,/dev/sd?,*blk?p?,/dev/*blk?p?
+# get_disk_from_partition partition
+get_disk_from_partition(){
+  local p="$1"
+  local disk=
+  expr "$p" : ".*p[[:digit:]][[:digit:]]*" >/dev/null && disk=${p%%p[0-9]*}
+  expr "$p" : ".*sd[[:alpha:]][[:digit:]][[:digit:]]*" >/dev/null && disk=${p%%[0-9]*}
+  if [[ -n "$disk" ]]; then
+    echo "$disk"
+    return 0
+  else
+    echo "$1"
+    return 1
+  fi
+}
+
+# fschuett
+# extract disk device names from start.conf partition definitions
+# get_disks
+get_disks(){
+  local parts="$(grep -i ^dev /start.conf | awk -F\= '{ print $2 }' | awk '{ print $1 }' )"
+  local disks=
+  for p in $parts; do
+    disks="$disks $(get_disk_from_partition "$p")"
+  done;
+  disks="$(echo $disks|tr " " "\n"|sort -u)"
+  echo "$disks"
+  return 0
+}
+
+
 # tschmitt
 # fetch fstype from start.conf
 # fstype_startconf dev
@@ -536,7 +568,7 @@ partition(){
  fi
 
  # grep all disks from start.conf
- local disks="$(grep -i ^dev /start.conf | awk -F\= '{ print $2 }' | awk '{ print $1 }' | sed -e 's|[0-9]*||g' | sort -u)"
+ local disks="$(get_disks)"
  # compute the last partition of each disk
  local i=""
  for i in $disks; do
@@ -545,7 +577,7 @@ partition(){
 
  while [ "$#" -ge "5" ]; do
   # support multiple disks
-  local disk="${1%%[1-9]*}"
+  local disk="$(get_disk_from_partition "$1")"
   if echo "$disks" | grep -q "$disk"; then
    disks="$(echo "$disks" | sed -e "s|$disk||")"
    local table=""
@@ -586,7 +618,7 @@ $(sfdisk -s "$disk" 2>> /tmp/linbo.log)
   fi
   let pcount++
   # Is this a primary partition?
-  local partno="${dev##?d?}"
+  local partno="$(expr "$dev" : ".*\([[:digit:]]\)")" # fix syntax highlighting: "
   if [ "$partno" -gt 4 ] >/dev/null 2>&1; then
    # Fill up unused partitions
    while [ "$pcount" < 5 ]; do
