204ad1ad1d0356325f554cf4b49e330c8d229f32
Thomas Schmitt <thomas@linuxmuster.net>
2016-11-11 11:25:36 +0100

    * linuxmuster-linbo7:   - Abhängigkeit zu linuxmuster-base7 angepasst.   - Konfiguration für atftpd entfernt.

--- files/lmn7/usr/lib/linuxmuster/setup.d/j_linbo.py
+++ files/lmn7/usr/lib/linuxmuster/setup.d/j_linbo.py
@@ -2,7 +2,7 @@
 #
 # j_linbo.py
 # thomas@linuxmuster.net
-# 20160916
+# 20161111
 #
 
 import configparser
@@ -20,42 +20,6 @@ i = configparser.ConfigParser()
 i.read(constants.SETUPINI)
 adminpw = i.get('setup', 'adminpw')
 serverip = i.get('setup', 'serverip')
-
-# atftpd options
-OPTIONS = '--daemon --bind-address ' + serverip + ' --tftpd-timeout 300 --retry-timeout 5 --maxthread 100 --verbose=5 ' + constants.LINBODIR
-
-# atftpd default configuration file
-configfile = '/etc/default/atftpd'
-
-# read configfile
-try:
-    with open(configfile, 'r') as infile:
-        filedata = infile.read()
-except:
-    print('Cannot read ' + configfile + '!')
-    exit(1)
-
-# replace old setup comment
-filedata = re.sub(r'# modified by linuxmuster-setup.*\n', '', filedata)
-
-# add newline at the end
-if not filedata[-1] == '\n':
-    filedata = filedata + '\n'
-
-# change options
-filedata = re.sub(r'\nUSE_INETD=.*\n', '\nUSE_INETD=false\n', filedata)
-filedata = re.sub(r'\nOPTIONS=.*\n', '\nOPTIONS="' + OPTIONS + '"\n', filedata)
-
-# set comment
-filedata = setupComment() + filedata
-
-# write configfile
-try:
-    with open(configfile, 'w') as outfile:
-        outfile.write(filedata)
-except:
-    print('Cannot write ' + configfile + '!')
-    exit(1)
 
 # rsyncd secrets
 configfile = '/etc/rsyncd.secrets'
@@ -76,7 +40,6 @@ os.system('chmod 600 ' + configfile)
 
 # restart services
 os.system('service rsync restart')
-os.system('service atftpd restart')
 
 # linbofs update
 os.system('update-linbofs')
