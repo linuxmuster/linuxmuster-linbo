7f0c6f944b1b277d227a487bf20ece19dd3d2bb3
fschuett <fschuett@gymnasium-himmelsthuer.de>
2017-04-12 13:05:13 +0200

    Vereinheitlichung durch kopieren von Dateien aus linbo/linbofs in das target

--- buildroot-external/board/post_build.sh
+++ buildroot-external/board/post_build.sh
@@ -23,6 +23,21 @@ rm -fv "${TARGET_DIR}"/usr/bin/pcretest
 
 # benötigte Verzeichnisse erstellen
 mkdir -p "${TARGET_DIR}"/cache
+mkdir -p "${TARGET_DIR}"/icons
+mkdir -p "${TARGET_DIR}"/linuxmuster-win
+mkdir -p "${TARGET_DIR}"/usr/share/udhcpc/default.script.d
 
-# create target copy of grub.cfg.local
+# copy templates to target
 cp -v "${BASE_DIR}"/../../share/templates/grub.cfg.local "${TARGET_DIR}"/usr/share/grub/grub.cfg
+# copy linbofs files to target
+LINBOFS_DIR="${BASE_DIR}"/../../linbofs
+cp -v "${LINBOFS_DIR}"/etc/newdev-patch.bvi "${TARGET_DIR}"/etc/newdev-patch.bvi
+cp -v "${LINBOFS_DIR}"/bin/patch_registry "${TARGET_DIR}"/usr/bin/
+cp -v "${LINBOFS_DIR}"/usr/bin/linbo_cmd.oss "${TARGET_DIR}"/usr/bin/linbo_cmd
+cp -v "${LINBOFS_DIR}"/usr/bin/linbo_wrapper.oss "${TARGET_DIR}"/usr/bin/linbo_wrapper
+cp -v "${LINBOFS_DIR}"/usr/share/udhcpc/default.script "${TARGET_DIR}"/usr/share/udhcpc/default.script.d/linbo.sh
+# copy linbo files to target
+LINBO_DIR="${BASE_DIR}"/../../linbo
+cp -v "${LINBO_DIR}"/icons/linbo_wallpaper_1024x768.png "${TARGET_DIR}"/icons/linbo_wallpaper.png
+cp -v "${LINBO_DIR}"/linuxmuster-win/* "${TARGET_DIR}"/linuxmuster-win/
+cp -v "${LINBO_DIR}"/german.kbd "${TARGET_DIR}"/usr/share/de-latin1-nodeadkeys.kmap
