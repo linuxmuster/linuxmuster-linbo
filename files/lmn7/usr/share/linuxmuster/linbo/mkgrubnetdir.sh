7860207f36237678213756e3b1ec2496654d830e
Thomas Schmitt <thomas@linuxmuster.net>
2017-07-25 13:35:11 +0000

    merge with master

--- files/lmn7/usr/share/linuxmuster/linbo/mkgrubnetdir.sh
+++ files/lmn7/usr/share/linuxmuster/linbo/mkgrubnetdir.sh
@@ -2,9 +2,12 @@
 #
 # creates directory structure for grub network boot
 # thomas@linuxmuster.net
-# 20170521
+# 20170529
 # GPL V3
 #
+
+# read linuxmuster environment
+. /usr/share/linuxmuster/defaults.sh || exit 1
 
 # required modules
 # common modules
@@ -30,39 +33,42 @@ EFI32="i386-efi"
 EFI64="x86_64-efi"
 
 # dirs
-NETDIR="debian/linuxmuster-linbo-common/var/linbo"
 SUBDIR="/boot/grub"
 I386_DIR="/usr/lib/grub/$I386"
 EFI32_DIR="/usr/lib/grub/$EFI32"
 EFI64_DIR="/usr/lib/grub/$EFI64"
 
 # image files
-CORE_I386="$NETDIR$SUBDIR/$I386/core"
-CORE_EFI32="$NETDIR$SUBDIR/$EFI32/core"
-CORE_EFI64="$NETDIR$SUBDIR/$EFI64/core"
+CORE_I386="$LINBOGRUBDIR/$I386/core"
+CORE_EFI32="$LINBOGRUBDIR/$EFI32/core"
+CORE_EFI64="$LINBOGRUBDIR/$EFI64/core"
 
 # grub.cfg templates
-GRUBCFG_TEMPLATE="files/common/usr/share/linuxmuster-linbo/templates/grub.cfg.pxe"
+GRUBCFG_TEMPLATE="$LINBOTPLDIR/grub.cfg.pxe"
 
 # fonts
 FONTS="unicode"
 
 # make cd/usb boot images (efi only)
-grub-mknetdir --modules="$GRUB_EFI32_ISO_MODULES" -d "$EFI32_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
-grub-mknetdir --modules="$GRUB_EFI64_ISO_MODULES" -d "$EFI64_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
+grub-mknetdir --modules="$GRUB_EFI32_ISO_MODULES" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --modules="$GRUB_EFI64_ISO_MODULES" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 mv "$CORE_EFI32.efi" "$CORE_EFI32.iso"
 mv "$CORE_EFI64.efi" "$CORE_EFI64.iso"
 
 # make special purpose minimal netboot image for i386
-grub-mknetdir --fonts="$FONTS" -d "$I386_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$FONTS" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 mv "$CORE_I386.0" "$CORE_I386.min"
 
 # make standard netboot images
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_I386_MODULES" -d "$I386_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI32_MODULES" -d "$EFI32_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI64_MODULES" -d "$EFI64_DIR" --net-directory="$NETDIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$FONTS" --modules="$GRUB_I386_MODULES" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI32_MODULES" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI64_MODULES" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 
 # copy remaining files
-rsync -a "$I386_DIR/" "$NETDIR$SUBDIR/$I386/"
-rsync -a "$EFI32_DIR/" "$NETDIR$SUBDIR/$EFI32/"
-rsync -a "$EFI64_DIR/" "$NETDIR$SUBDIR/$EFI64/"
+rsync -a "$I386_DIR/" "$LINBOGRUBDIR/$I386/"
+rsync -a "$EFI32_DIR/" "$LINBOGRUBDIR/$EFI32/"
+rsync -a "$EFI64_DIR/" "$LINBOGRUBDIR/$EFI64/"
+
+# copy ipxe files
+cp /boot/ipxe.lkrn "$LINBOGRUBDIR"
+cp /boot/ipxe.efi "$LINBOGRUBDIR"
