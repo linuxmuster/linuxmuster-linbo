39825d0638ae6211ef6c9b4f5c8e4d4b0dcd86ad
Thomas Schmitt <thomas@linuxmuster.net>
2017-07-26 16:44:59 +0000

    * debian/rules: fixed file distribution in linuxmuster-linbo-common7 pkg. * linuxmuster-linbo7:   - added script mkgrubhostimg.py for host specific pxe boot image creation.   - removed module variables in mkgrubnetdir.sh using global variables from     linuxmuster-base7.

--- files/lmn7/usr/share/linuxmuster/linbo/mkgrubnetdir.sh
+++ files/lmn7/usr/share/linuxmuster/linbo/mkgrubnetdir.sh
@@ -2,30 +2,12 @@
 #
 # creates directory structure for grub network boot
 # thomas@linuxmuster.net
-# 20170529
+# 20170726
 # GPL V3
 #
 
 # read linuxmuster environment
 . /usr/share/linuxmuster/defaults.sh || exit 1
-
-# required modules
-# common modules
-GRUB_COMMON_MODULES="all_video chain configfile cpuid echo net ext2 extcmd fat gettext gfxmenu gfxterm http \
- ntfs linux loadenv minicmd net part_gpt part_msdos png progress reiserfs search terminal test tftp"
-
-# modules needed for cd/usb boot
-GRUB_ISO_MODULES="iso9660 usb"
-
-# arch specific netboot modules
-GRUB_EFI32_MODULES="$GRUB_COMMON_MODULES efi_gop efi_uga efinet"
-GRUB_EFI64_MODULES="$GRUB_COMMON_MODULES efi_gop efi_uga efinet linuxefi"
-#GRUB_PC_MODULES="$GRUB_COMMON_MODULES biosdisk ntldr pxe"
-GRUB_I386_MODULES="$GRUB_COMMON_MODULES biosdisk ntldr pxe"
-
-# arch specific cd/usb boot modules (grub-pc not needed, boots with syslinux)
-GRUB_EFI32_ISO_MODULES="$GRUB_ISO_MODULES $GRUB_EFI32_MODULES"
-GRUB_EFI64_ISO_MODULES="$GRUB_ISO_MODULES $GRUB_EFI64_MODULES"
 
 # architectures
 I386="i386-pc"
@@ -50,19 +32,19 @@ GRUBCFG_TEMPLATE="$LINBOTPLDIR/grub.cfg.pxe"
 FONTS="unicode"
 
 # make cd/usb boot images (efi only)
-grub-mknetdir --modules="$GRUB_EFI32_ISO_MODULES" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
-grub-mknetdir --modules="$GRUB_EFI64_ISO_MODULES" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --modules="$GRUBEFIMODS $GRUBISOMODS" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --modules="$GRUBEFIMODS $GRUBISOMODS" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 mv "$CORE_EFI32.efi" "$CORE_EFI32.iso"
 mv "$CORE_EFI64.efi" "$CORE_EFI64.iso"
 
 # make special purpose minimal netboot image for i386
-grub-mknetdir --fonts="$FONTS" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$GRUBFONT" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 mv "$CORE_I386.0" "$CORE_I386.min"
 
 # make standard netboot images
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_I386_MODULES" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI32_MODULES" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
-grub-mknetdir --fonts="$FONTS" --modules="$GRUB_EFI64_MODULES" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$GRUBFONT" --modules="$GRUBI386MODS" -d "$I386_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$GRUBFONT" --modules="$GRUBEFIMODS" -d "$EFI32_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
+grub-mknetdir --fonts="$GRUBFONT" --modules="$GRUBEFIMODS" -d "$EFI64_DIR" --net-directory="$LINBODIR" --subdir="$SUBDIR"
 
 # copy remaining files
 rsync -a "$I386_DIR/" "$LINBOGRUBDIR/$I386/"
