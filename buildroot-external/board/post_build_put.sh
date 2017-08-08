794d8585a5d8d09a4e640983ea2d00831f56f3c6
fschuett <fschuett@gymnasium-himmelsthuer.de>
2017-08-08 18:39:23 +0200

    fix linbo_wrapper access rights

--- buildroot-external/board/post_build_put.sh
+++ buildroot-external/board/post_build_put.sh
@@ -17,7 +17,8 @@ cp -v "${LINBOFS_DIR}"/etc/linbo-version "${TARGET_DIR}"/etc/
 cp -v "${LINBOFS_DIR}"/etc/newdev-patch.bvi "${TARGET_DIR}"/etc/newdev-patch.bvi
 cp -v "${LINBOFS_DIR}"/bin/patch_registry "${TARGET_DIR}"/usr/bin/
 cp -v "${LINBOFS_DIR}"/usr/share/udhcpc/default.script "${TARGET_DIR}"/usr/share/udhcpc/default.script.d/linbo.sh
-sed 's@#!/bin/sh@#!/usr/bin/ash@' "${LINBOFS_DIR}"/usr/bin/linbo_wrapper >"${TARGET_DIR}"/usr/bin/linbo_wrapper
+cp -v "${LINBOFS_DIR}"/usr/bin/linbo_wrapper "${TARGET_DIR}"/usr/bin/linbo_wrapper
+sed 's@#!/bin/sh@#!/usr/bin/ash@' -i "${TARGET_DIR}"/usr/bin/linbo_wrapper
 # copy linbo files to target
 LINBO_DIR="${BASE_DIR}"/../../linbo
 cp -v "${LINBO_DIR}"/icons/linbo_wallpaper_1024x768.png "${TARGET_DIR}"/icons/linbo_wallpaper.png
