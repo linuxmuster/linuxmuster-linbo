2a07ca4acc3dd6c4b5b16036bc7d0391a01fcc4c
Frank Schütte <fschuett@gymhim.de>
2018-04-08 19:28:30 +0200

    add hash, fix build

--- buildroot-external/package/ubuntugrub/ubuntugrub.mk
+++ buildroot-external/package/ubuntugrub/ubuntugrub.mk
@@ -5,10 +5,11 @@
 ################################################################################
 
 UBUNTUGRUB_VERSION = 2.02
-UBUNTUGRUB_SITE = $(TOPDIR)/../rpm/grub-bin.tar.gz
+UBUNTUGRUB_SITE = $(TOPDIR)/../rpm
+UBUNTUGRUB_SOURCE = grub-bin.tar.gz
 UBUNTUGRUB_SITE_METHOD = file
 UBUNTUGRUB_ACTUAL_SOURCE_TARBALL = grub-$(UBUNTUGRUB_VERSION).tar.gz
-UBUNTUGRUB_ACTUAL_SOURCE_SITE = https://ftp.gnu.org/gnu/grub/
+UBUNTUGRUB_ACTUAL_SOURCE_SITE = https://ftp.gnu.org/gnu/grub
 UBUNTUGRUB_LICENSE = GPLv3
 UBUNTUGRUB_LICENSE_FILES = COPYING
 
@@ -20,31 +21,37 @@ define UBUNTUGRUB_BUILD_CMDS
 	echo "Nothing to do"
 endef
 
+ifeq ($(BR2_x86_64),y)
 define UBUNTUGRUB_INSTALL_TARGET_CMDS
-	cp -avR $(@D)/* $(TARGET_DIR)/
-ifeq ($(BR2_x86_64,y))
-	  cp $(TARGET_DIR)/usr/bin64/* $(TARGET_DIR)/usr/bin
-else
-	  cp $(TARGET_DIR)/usr/bin32/* $(TARGET_DIR)/usr/bin
-	  rm -rf $(TARGET_DIR)/usr/lib/x86_64-pc
-	  rm -rf $(TARGET_DIR)/usr/lib/x86_86-efi
-endif
-	rm -rf $(TARGET_DIR)/usr/bin32 $(TARGET_DIR)/usr/bin64
+    mkdir -p $(TARGET_DIR)/usr
+    cp -R $(@D)/* $(TARGET_DIR)/usr
+    cp $(TARGET_DIR)/usr/bin64/* $(TARGET_DIR)/usr/bin
+    rm -rf $(TARGET_DIR)/usr/bin32 $(TARGET_DIR)/usr/bin64
 endef
+else
+define UBUNTUGRUB_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr
+    cp -R $(@D)/* $(TARGET_DIR)/usr
+    cp $(TARGET_DIR)/usr/bin32/* $(TARGET_DIR)/usr/bin
+    rm -rf $(TARGET_DIR)/usr/lib/x86_64-pc
+    rm -rf $(TARGET_DIR)/usr/lib/x86_64-efi
+    rm -rf $(TARGET_DIR)/usr/bin32 $(TARGET_DIR)/usr/bin64
+endef
+endif
 
 ifeq ($(BR2_x86_64),y)
 UBUNTUGRUB_IMGS = boot boot_hybrid cdboot diskboot kernel lnxboot lzma_decompress pxeboot
 UBUNTUGRUB_MODS = \
-	acpi adler32 affs afs ahci all_video aout archelp ata at_keyboard backtrace bfs biosdisk bitmap bitmap_scale \
+	915resolution acpi adler32 affs afs ahci all_video aout appleldr archelp ata at_keyboard backtrace bfs biosdisk bitmap bitmap_scale \
 	blocklist boot bsd bswap_test btrfs bufio cat cbfs cbls cbmemc cbtable cbtime \
 	chain cmdline_cat_test cmosdump cmostest cmp cmp_test configfile cpio_be cpio cpuid crc64 cryptodisk crypto \
-	cs5536 ctz_test datehook date datetime diskfilter disk div div_test dm_nv drivemap echo efiemu ehci elf eval \
-	exfat exfctest ext2 extcmd fat file font freedos fshelp functional_test gcry_arcfour gcry_blowfish gcry_camellia \
+	cs5536 ctz_test datehook date datetime diskfilter disk div div_test dm_nv drivemap echo efinet efi_gop efi_uga efiemu ehci elf eval \
+	efifwsetup exfat exfctest ext2 extcmd fat file fixvideo font freedos fshelp functional_test gcry_arcfour gcry_blowfish gcry_camellia \
 	gcry_cast5 gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5 gcry_rfc2268 gcry_rijndael gcry_rmd160 \
 	gcry_rsa gcry_seed gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger gcry_twofish gcry_whirlpool \
 	gdb geli gettext gfxmenu gfxterm_background gfxterm_menu gfxterm gptsync gzio halt hashsum hdparm hello help \
-	hexdump hfs hfspluscomp hfsplus http iorw iso9660 jfs jpeg keylayouts keystatus ldm legacycfg legacy_password_test \
-	linux16 linux loadenv loopback lsacpi lsapm lsmmap ls lspci luks lvm lzopio macbless macho mda_text mdraid09_be \
+	hexdump hfs hfspluscomp hfsplus http hwmatch iorw iso9660 jfs jpeg keylayouts keystatus ldm legacycfg legacy_password_test \
+	linux16 linux linuxefi loadbios loadenv loopback lsacpi lsapm lsefi lsefimmap lsefisystab lsmmap ls lspci lssal luks lvm lzopio macbless macho mda_text mdraid09_be \
 	mdraid09 mdraid1x memdisk memrw minicmd minix2_be minix2 minix3_be minix3 minix_be minix mmap morse mpi msdospart \
 	mul_test multiboot2 multiboot nativedisk net newc nilfs2 normal ntfscomp ntfs ntldr odc offsetio ohci part_acorn \
 	part_amiga part_apple part_bsd part_dfly part_dvh part_gpt part_msdos part_plan part_sun part_sunpc parttool \
@@ -58,8 +65,13 @@ UBUNTUGRUB_MODS = \
 UBUNTUGRUB_CHECK_BIN_ARCH_EXCLUSIONS = \
 	$(patsubst %,/usr/lib/grub/i386-pc/%.mod,$(UBUNTUGRUB_MODS)) \
 	$(patsubst %,/usr/lib/grub/i386-pc/%.img,$(UBUNTUGRUB_IMGS)) \
+	/usr/lib/grub/i386-pc/efiemu32.o \
 	$(patsubst %,/usr/lib/grub/i386-efi/%.mod,$(UBUNTUGRUB_MODS)) \
 	$(patsubst %,/usr/lib/grub/i386-efi/%.img,$(UBUNTUGRUB_IMGS))
+endif
+
+ifeq ($(BR2_x86_64),y)
+HOST_UBUNTUGRUB_DEPENDENCIES = host-lvm2
 endif
 
 HOST_UBUNTUGRUB_MODS = all_video boot chain configfile cpuid echo net ext2 extcmd fat \
@@ -69,12 +81,13 @@ HOST_UBUNTUGRUB_MODS = all_video boot chain configfile cpuid echo net ext2 extcm
 
 HOST_UBUNTUGRUB_FONT = unicode
 
-# Grub2 install unicode font
-define HOST_UBUNTUGRUB_INSTALLATION_CMDS
-ifeq ($(BR2_x86_64,y))
-	cp -avR $(@D)/* $(HOST_DIR)/
+ifeq ($(BR2_x86_64),y)
+define HOST_UBUNTUGRUB_INSTALL_CMDS
+	mkdir -p $(HOST_DIR)/usr
+	cp -R $(@D)/* $(HOST_DIR)/usr
 	cp $(HOST_DIR)/usr/bin64/* $(HOST_DIR)/usr/bin
 	rm -rf $(HOST_DIR)/usr/bin32 $(HOST_DIR)/usr/bin64
+# Grub2 install unicode font
 	cp $(BASE_DIR)/../../linbofs/usr/share/grub/$(HOST_UBUNTUGRUB_FONT).pf2 $(HOST_DIR)/usr/share/grub/
 # Grub2 BIOS netdir creation
 	mkdir -p $(BASE_DIR)/boot/grub
@@ -119,8 +132,12 @@ ifeq ($(BR2_x86_64,y))
 		--subdir=/boot/grub \
 		--modules="$(HOST_UBUNTUGRUB_MODS)" \
 		-d $(HOST_DIR)/lib/grub/x86_64-efi
-endif
 endef
+else
+define HOST_UBUNTUGRUB_INSTALL_CMDS
+	echo "Nothing to do."
+endef
+endif
 
 $(eval $(generic-package))
 $(eval $(host-generic-package))
