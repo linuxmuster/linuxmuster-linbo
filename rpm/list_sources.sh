bda3dfb453d4b7b23c4ce378e7571ce17716e269
Frank Schütte <fschuett@gymhim.de>
2018-11-05 10:20:04 +0100

    fix list_sources.sh, new package versions for buildroot-2018.08.x

--- rpm/list_sources.sh
+++ rpm/list_sources.sh
@@ -6,12 +6,13 @@
 # GPL v3.0+
 #
 
-DOWNLOADS=$(find . -name '*.gz' -or -name '*.bz2' -or -name '*.tgz' -or -name '*.zip' -or -name '*.xz' -or -name '*.patch' |sort)
+DOWNLOADS=$(find . -maxdepth 1 -name '*.gz' -or -name '*.bz2' -or -name '*.tgz' -or -name '*.zip' -or -name '*.xz' -or -name '*.patch'|grep -v oss-linbo |sort)
 
 let num=1
 
 for f in $DOWNLOADS; do
-  echo "Source${num}:	${f#./}"
+  f=${f#./}
+  echo "Source${num}:	$f"
   (( num = num + 1 ))
 done
 
