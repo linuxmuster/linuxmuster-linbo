28520656ddb8de23f1455a3b7f1c0c5d7f645971
Thomas Schmitt <thomas@linuxmuster.net>
2019-03-27 16:59:47 +0100

    - linbofs, linbo_cmd:   - minor code beautifying.   - Fix #108: sync+start fails with windows systems in offline mode. - lmn7:   - Fix #89: update-linbofs warns and corrects unsecure permissions of ssh files, which differ from the standard.   - postinst: Apply current server ip to start.conf examples on configured systems. - lmn62: removed PubkeyAcceptedKeyTypes=+ssh-dss from ssh_config since it caused a bad configuration error.

--- files/lmn7/usr/share/linuxmuster/linbo/update-linbofs.sh
+++ files/lmn7/usr/share/linuxmuster/linbo/update-linbofs.sh
@@ -6,7 +6,7 @@
 #
 # thomas@linuxmuster.net
 # GPL V3
-# 20170130
+# 20190327
 #
 
 # read linuxmuster environment
@@ -44,6 +44,7 @@ update_linbofs() {
  local linbofscachedir="$LINBOCACHEDIR/linbofs$suffix"
  local linbofs="$LINBODIR/linbofs${suffix}.lz"
  local linbofs_md5="$linbofs".md5
+ local i
  rm -f "$linbofs_md5"
  rm -rf "$linbofscachedir"
  mkdir -p "$linbofscachedir"
@@ -71,6 +72,17 @@ update_linbofs() {
  cat /root/.ssh/id_*.pub > .ssh/authorized_keys
  mkdir -p var/log
  touch var/log/lastlog
+ # check and repair permissions
+ for i in .ssh .ssh/authorized_keys; do
+  perms="$(LANG=C stat "$i" | grep ^Access | grep Uid: | awk -F\( '{ print $2 }' | awk -F\/ '{ print $1 }')"
+	if [ "${perms:1:3}" = "666" -o "${perms:1:3}" = "777" ]; then
+	 echo "WARNING! $i has bogus permissions!"
+	 sleep 3
+	 echo "Repairing for now but check your filesystem!"
+	 [ -d "$i" ] && chmod 755 "$i"
+	 [ -f "$i" ] && chmod 644 "$i"
+  fi
+ done
 
  # copy default start.conf
  cp -f $LINBODIR/start.conf .
