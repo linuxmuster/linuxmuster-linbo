dd43fd90b2fbd8f8c7144447056359050a6c9f19
Thomas Schmitt <thomas@linuxmuster.net>
2020-02-03 18:12:16 +0100

    - grub console output fix on operating system boot. - fix time sync output.

--- linbofs/init.sh
+++ linbofs/init.sh
@@ -5,7 +5,7 @@
 # License: GPL V2
 #
 # thomas@linuxmuster.net
-# 20191206
+# 20200203
 #
 
 # If you don't have a "standalone shell" busybox, enable this:
@@ -618,9 +618,9 @@ network(){
   if [ -s /start.conf ]; then
    echo "Network connection to $server established successfully."
    grep ^[a-z] /tmp/dhcp.log | sed -e 's|^|local |g' > /tmp/network.ok
-   echo "Syncing time from server:"
-   ntpd -n -q -p "$server"
-   hwclock --systohc
+   echo "Syncing time ..."
+   ntpd -n -q -p "$server" &> /dev/null
+   hwclock --systohc &> /dev/null
    date
    # linbo update & grub installation
    do_linbo_update "$server"
