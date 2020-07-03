e6d5c1bf69a399c3525664186dc1bc1b30e20128
Frank Schütte <fschuett@gymhim.de>
2020-07-03 22:04:38 +0200

    adapt to buildroot-2020.05

--- rpm/list_sources.sh
+++ rpm/list_sources.sh
@@ -6,12 +6,13 @@
 # GPL v3.0+
 #
 
-DOWNLOADS=$(find . -maxdepth 1 -name '*.gz' -or -name '*.bz2' -or -name '*.tgz' -or -name '*.zip' -or -name '*.xz' -or -name '*.patch'|grep -v oss-linbo |sort)
+DOWNLOADS=$(find . -maxdepth 2 -name '*.gz' -or -name '*.bz2' -or -name '*.tgz' -or -name '*.zip' -or -name '*.xz' -or -name '*.patch'|grep -v oss-linbo |sort)
 
 let num=1
 
 for f in $DOWNLOADS; do
-  f=${f#./}
+  cp $f ${f##*/}
+  f=${f##*/}
   echo "Source${num}:	$f"
   (( num = num + 1 ))
 done
