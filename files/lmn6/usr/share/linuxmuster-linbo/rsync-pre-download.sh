75bf9f7cca19ab67e08e4ca1f60ce91975911002
Thomas Schmitt <thomas@linuxmuster.net>
2020-11-11 14:11:59 +0100

    more english translations of console messages and further code beautifying.

--- files/lmn6/usr/share/linuxmuster-linbo/rsync-pre-download.sh
+++ files/lmn6/usr/share/linuxmuster-linbo/rsync-pre-download.sh
@@ -38,155 +38,155 @@ echo "EXT: $EXT"
 
 case $EXT in
 
- # handle machine account password
-# *.mpw)
-  # set machine account password
-#  MACHINEPW="12345678"
-#  echo -e "${MACHINEPW}\n${MACHINEPW}\n" | smbpasswd -e -s "$compname$"
-#  MACHINEPW="$(pwgen -s 10 1)"
-#  if ! sophomorix-passwd --force --user "$compname$" --pass "$MACHINEPW" | grep -qi "error"; then
-#   echo "$MACHINEPW" > "$FILE"
-#  fi
- *.macct)
-  LDAPMODIFY="$(which ldapmodify)"
-  LDAPSEARCH="$(which ldapsearch)"
-  imagemacct="$LINBODIR/${RSYNC_REQUEST##*/}"
-  ldapsec="/etc/ldap.secret"
-  # upload samba machine password hashes to host's machine account
-  if [ -s "$imagemacct" -a -n "$basedn" ]; then
-   echo "Machine account file: $imagemacct"
-   echo "Host: $compname"
-   echo "Writing samba machine password hashes to ldap account:"
-   sed -e "s|@@compname@@|$compname|" "$imagemacct" | "$LDAPMODIFY" -x -y "$ldapsec" -D "cn=admin,$basedn" -h localhost
-   # check for success
-   sambaNTpwhash_cur="$("$LDAPSEARCH" -y "$ldapsec" -D cn=admin,$basedn -x -h localhost "(uid=$compname$)" sambaNTPassword | grep ^sambaNTPassword: | awk '{ print $2 }')"
-   sambaNTpwhash_new="$(grep ^sambaNTPassword: "$imagemacct" | awk '{ print $2 }')"
-   if [ "$sambaNTpwhash_new" != "$sambaNTpwhash_cur" ]; then
-    echo "Not successfull, once again:"
-    sed -e "s|@@compname@@|$compname|" "$imagemacct" | "$LDAPMODIFY" -x -y "$ldapsec" -D "cn=admin,$basedn" -h localhost
-   fi
-  fi
- ;;
+    # handle machine account password
+    # *.mpw)
+    # set machine account password
+    #  MACHINEPW="12345678"
+    #  echo -e "${MACHINEPW}\n${MACHINEPW}\n" | smbpasswd -e -s "$compname$"
+    #  MACHINEPW="$(pwgen -s 10 1)"
+    #  if ! sophomorix-passwd --force --user "$compname$" --pass "$MACHINEPW" | grep -qi "error"; then
+    #   echo "$MACHINEPW" > "$FILE"
+    #  fi
+  *.macct)
+    LDAPMODIFY="$(which ldapmodify)"
+    LDAPSEARCH="$(which ldapsearch)"
+    imagemacct="$LINBODIR/${RSYNC_REQUEST##*/}"
+    ldapsec="/etc/ldap.secret"
+    # upload samba machine password hashes to host's machine account
+    if [ -s "$imagemacct" -a -n "$basedn" ]; then
+      echo "Machine account file: $imagemacct"
+      echo "Host: $compname"
+      echo "Writing samba machine password hashes to ldap account:"
+      sed -e "s|@@compname@@|$compname|" "$imagemacct" | "$LDAPMODIFY" -x -y "$ldapsec" -D "cn=admin,$basedn" -h localhost
+      # check for success
+      sambaNTpwhash_cur="$("$LDAPSEARCH" -y "$ldapsec" -D cn=admin,$basedn -x -h localhost "(uid=$compname$)" sambaNTPassword | grep ^sambaNTPassword: | awk '{ print $2 }')"
+      sambaNTpwhash_new="$(grep ^sambaNTPassword: "$imagemacct" | awk '{ print $2 }')"
+      if [ "$sambaNTpwhash_new" != "$sambaNTpwhash_cur" ]; then
+        echo "Not successfull, once again:"
+        sed -e "s|@@compname@@|$compname|" "$imagemacct" | "$LDAPMODIFY" -x -y "$ldapsec" -D "cn=admin,$basedn" -h localhost
+      fi
+    fi
+    ;;
 
- # fetch logfiles from client
- *.log)
-  host_logfile="$(basename "$FILE")"
-  echo "Upload request for $host_logfile."
-  src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
-  tgt_logfile="$LINBOLOGDIR/$host_logfile"
-  linbo-scp -v "${RSYNC_HOST_NAME}:$src_logfile" "$FILE" || RC="1"
-  if [ -s "$FILE" ]; then
-   echo "## Log session begin: $(date) ##" >> "$tgt_logfile"
-   cat "$FILE" >> "$tgt_logfile"
-   echo "## Log session end: $(date) ##" >> "$tgt_logfile"
-  fi
-  rm -f "$FILE"
-  touch "$FILE"
- ;;
+    # fetch logfiles from client
+  *.log)
+    host_logfile="$(basename "$FILE")"
+    echo "Upload request for $host_logfile."
+    src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
+    tgt_logfile="$LINBOLOGDIR/$host_logfile"
+    linbo-scp -v "${RSYNC_HOST_NAME}:$src_logfile" "$FILE" || RC="1"
+    if [ -s "$FILE" ]; then
+      echo "## Log session begin: $(date) ##" >> "$tgt_logfile"
+      cat "$FILE" >> "$tgt_logfile"
+      echo "## Log session end: $(date) ##" >> "$tgt_logfile"
+    fi
+    rm -f "$FILE"
+    touch "$FILE"
+    ;;
 
-   # fetch image status from client
- *.status)
-   host_logfile="$(basename "$FILE")"
-   echo "Upload request for $host_logfile."
-   src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
-   tgt_logfile="$LINBOLOGDIR/$host_logfile"
-   linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
-   if [ -s "$FILE" ]; then
-     if [ -s "$tgt_logfile" ]; then
-       # remove older entry for image
-       image="$(cat "$FILE" | awk '{ print $3 }')"
-       [ -n "$image" ] && sed -i "/$image/d" "$tgt_logfile"
-     fi
-     cat "$FILE" >> "$tgt_logfile"
-   fi
-   rm -f "$FILE"
-   touch "$FILE"
-   ;;
+    # fetch image status from client
+  *.status)
+    host_logfile="$(basename "$FILE")"
+    echo "Upload request for $host_logfile."
+    src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
+    tgt_logfile="$LINBOLOGDIR/$host_logfile"
+    linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
+    if [ -s "$FILE" ]; then
+      if [ -s "$tgt_logfile" ]; then
+        # remove older entry for image
+        image="$(cat "$FILE" | awk '{ print $3 }')"
+        [ -n "$image" ] && sed -i "/$image/d" "$tgt_logfile"
+      fi
+      cat "$FILE" >> "$tgt_logfile"
+    fi
+    rm -f "$FILE"
+    touch "$FILE"
+    ;;
 
- # provide host's opsi key for download
- *.opsikey)
-  # invoked by linbo_cmd on postsync
-  # if opsi server is configured and host is opsimanaged
-  if ([ -n "$opsiip" ] && opsimanaged "$compname"); then
-   echo "Opsi key file $(basename $FILE) requested."
-   key="$(grep ^"$RSYNC_HOST_NAME" "$LINBOOPSIKEYS" | awk -F\: '{ print $2 }')"
-   if [ -n "$key" ]; then
-    echo "Opsi key for $RSYNC_HOST_NAME found, providing key file."
-    echo "$key" > "$FILE"
-    chmod 644 "$FILE"
-    # upload opsiip to client
-    linbo-ssh "$compname" "echo $opsiip > /tmp/opsiip"
-    # get opsi server cert and provide it to client
-    opsipem="opsiconfd.pem"
-    rsync -v "$opsiip:/etc/opsi/$opsipem" "$LINBODIR/$opsipem"
-    chmod 600 "$LINBODIR/$opsipem"
-    linbo-scp -v "$LINBODIR/$opsipem" "$compname:/tmp"
-   fi
-  fi
- ;;
+    # provide host's opsi key for download
+  *.opsikey)
+    # invoked by linbo_cmd on postsync
+    # if opsi server is configured and host is opsimanaged
+    if ([ -n "$opsiip" ] && opsimanaged "$compname"); then
+      echo "Opsi key file $(basename $FILE) requested."
+      key="$(grep ^"$RSYNC_HOST_NAME" "$LINBOOPSIKEYS" | awk -F\: '{ print $2 }')"
+      if [ -n "$key" ]; then
+        echo "Opsi key for $RSYNC_HOST_NAME found, providing key file."
+        echo "$key" > "$FILE"
+        chmod 644 "$FILE"
+        # upload opsiip to client
+        linbo-ssh "$compname" "echo $opsiip > /tmp/opsiip"
+        # get opsi server cert and provide it to client
+        opsipem="opsiconfd.pem"
+        rsync -v "$opsiip:/etc/opsi/$opsipem" "$LINBODIR/$opsipem"
+        chmod 600 "$LINBODIR/$opsipem"
+        linbo-scp -v "$LINBODIR/$opsipem" "$compname:/tmp"
+      fi
+    fi
+    ;;
 
- # handle windows product key request
- *.winkey)
-  # get key from workstations and write it to temporary file
-  if [ -n "$compname" ]; then
-   winkey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $7 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
-   officekey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $6 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
-   [ -n "$winkey" ] && echo "winkey=$winkey" > "$FILE"
-   [ -n "$officekey" ] && echo "officekey=$officekey" >> "$FILE"
-  fi
- ;;
+    # handle windows product key request
+  *.winkey)
+    # get key from workstations and write it to temporary file
+    if [ -n "$compname" ]; then
+      winkey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $7 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
+      officekey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $6 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
+      [ -n "$winkey" ] && echo "winkey=$winkey" > "$FILE"
+      [ -n "$officekey" ] && echo "officekey=$officekey" >> "$FILE"
+    fi
+    ;;
 
- # handle windows activation tokens archive
- winact-upload)
-  RC=0
-  FILE="${FILE%.upload}"
-  # fetch archive from client
-  echo "Upload request for windows activation tokens archive."
-  linbo-scp "${RSYNC_HOST_NAME}:/cache/$(basename $FILE)" "${FILE}.tmp" || RC="1"
-  # if archive file already exists try to merge old and new archives
-  if [ -s "$FILE" -a "$RC" = "0" ]; then
-   echo "Updating existing archive $FILE."
-   tmpdir="/var/tmp/winact-upload.$$"
-   curdir="$(pwd)"
-   mkdir -p "$tmpdir"
-   # extract old archive to tmpdir
-   tar xf "$FILE" -C "$tmpdir" || RC="1"
-   if [ "$RC" = "0" ]; then
-    # extract uploaded archive over old archive in tmpdir
-    tar xf "${FILE}.tmp" -C "$tmpdir" || RC="1"
-   fi
-   if [ "$RC" = "0" ]; then
-    rm -f "${FILE}.tmp"
-    cd "$tmpdir"
-    # pack content of tmpdir to temporary archive
-    tar czf "${FILE}.tmp" * || RC="1"
-    cd "$curdir"
-   fi
-   rm -rf "$tmpdir"
-  fi
-  # move uploaded file in place
-  if [ "$RC" = "0" ]; then
-   rm -f "$FILE"
-   mv "${FILE}.tmp" "$FILE" || RC="1"
-  fi
-  if [ "$RC" = "0" ]; then
-   echo "Upload of $FILE successfully finished."
-  else
-   echo "Sorry. Upload of $FILE failed."
-  fi
-  rm -f "$PIDFILE"
- ;;
+    # handle windows activation tokens archive
+  winact-upload)
+    RC=0
+    FILE="${FILE%.upload}"
+    # fetch archive from client
+    echo "Upload request for windows activation tokens archive."
+    linbo-scp "${RSYNC_HOST_NAME}:/cache/$(basename $FILE)" "${FILE}.tmp" || RC="1"
+    # if archive file already exists try to merge old and new archives
+    if [ -s "$FILE" -a "$RC" = "0" ]; then
+      echo "Updating existing archive $FILE."
+      tmpdir="/var/tmp/winact-upload.$$"
+      curdir="$(pwd)"
+      mkdir -p "$tmpdir"
+      # extract old archive to tmpdir
+      tar xf "$FILE" -C "$tmpdir" || RC="1"
+      if [ "$RC" = "0" ]; then
+        # extract uploaded archive over old archive in tmpdir
+        tar xf "${FILE}.tmp" -C "$tmpdir" || RC="1"
+      fi
+      if [ "$RC" = "0" ]; then
+        rm -f "${FILE}.tmp"
+        cd "$tmpdir"
+        # pack content of tmpdir to temporary archive
+        tar czf "${FILE}.tmp" * || RC="1"
+        cd "$curdir"
+      fi
+      rm -rf "$tmpdir"
+    fi
+    # move uploaded file in place
+    if [ "$RC" = "0" ]; then
+      rm -f "$FILE"
+      mv "${FILE}.tmp" "$FILE" || RC="1"
+    fi
+    if [ "$RC" = "0" ]; then
+      echo "Upload of $FILE successfully finished."
+    else
+      echo "Sorry. Upload of $FILE failed."
+    fi
+    rm -f "$PIDFILE"
+    ;;
 
- # prepare download of local grub.cfg
- grub-local)
-  grubcfg_tpl="$LINBOTPLDIR/grub.cfg.local"
-  group="$(basename "$FILE" | awk -F\. '{ print $2 }')"
-  startconf="$LINBODIR/start.conf.$group"
-  append="$(linbo_kopts "$startconf") localboot"
-  sed -e "s|linux \$linbo_kernel .*|linux \$linbo_kernel $append|g" "$grubcfg_tpl" > "$FILE"
- ;;
+    # prepare download of local grub.cfg
+  grub-local)
+    grubcfg_tpl="$LINBOTPLDIR/grub.cfg.local"
+    group="$(basename "$FILE" | awk -F\. '{ print $2 }')"
+    startconf="$LINBODIR/start.conf.$group"
+    append="$(linbo_kopts "$startconf") localboot"
+    sed -e "s|linux \$linbo_kernel .*|linux \$linbo_kernel $append|g" "$grubcfg_tpl" > "$FILE"
+    ;;
 
- *) ;;
+  *) ;;
 
 esac
 
--- files/lmn7/usr/share/linuxmuster/linbo/rsync-pre-download.sh
+++ files/lmn7/usr/share/linuxmuster/linbo/rsync-pre-download.sh
@@ -39,158 +39,158 @@ echo "EXT: $EXT"
 
 case $EXT in
 
- # handle machine account password
- *.macct)
-  url="--url=/var/lib/samba/private/sam.ldb"
-  LDBSEARCH="$(which ldbsearch) $url"
-  LDBMODIFY="$(which ldbmodify) $url"
-  imagemacct="$LINBODIR/${RSYNC_REQUEST##*/}"
-  # upload samba machine password hashes to host's ad machine account
-  if [ -s "$imagemacct" ]; then
-   echo "Machine account ldif file: $imagemacct"
-   echo "Host: $compname"
-   # get dn of host
-   dn="$($LDBSEARCH "(&(sAMAccountName=$compname$))" | grep ^dn | awk '{ print $2 }')"
-   if [ -n "$dn" ]; then
-    echo "DN: $dn"
-    ldif="/var/tmp/${compname}_macct.$$"
-    ldbopts="--nosync --verbose --controls=relax:0 --controls=local_oid:1.3.6.1.4.1.7165.4.3.7:0 --controls=local_oid:1.3.6.1.4.1.7165.4.3.12:0"
-    sed -e "s|@@dn@@|$dn|" "$imagemacct" > "$ldif"
-    $LDBMODIFY $ldbopts "$ldif"
-    rm -f "$ldif"
-   else
-    echo "Cannot determine DN of $compname! Aborting!"
-   fi
-  fi
- ;;
-
- # fetch logfiles from client
- *.log)
-  host_logfile="$(basename "$FILE")"
-  echo "Upload request for $host_logfile."
-  src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
-  tgt_logfile="$LINBOLOGDIR/$host_logfile"
-  linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
-  if [ -s "$FILE" ]; then
-   echo "## Log session begin: $(date) ##" >> "$tgt_logfile"
-   cat "$FILE" >> "$tgt_logfile"
-   echo "## Log session end: $(date) ##" >> "$tgt_logfile"
-  fi
-  rm -f "$FILE"
-  touch "$FILE"
- ;;
-
-  # fetch image status from client
-*.status)
-  host_logfile="$(basename "$FILE")"
-  echo "Upload request for $host_logfile."
-  src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
-  tgt_logfile="$LINBOLOGDIR/$host_logfile"
-  linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
-  if [ -s "$FILE" ]; then
-    if [ -s "$tgt_logfile" ]; then
-      # remove older entry for image
-      image="$(cat "$FILE" | awk '{ print $3 }')"
-      [ -n "$image" ] && sed -i "/$image/d" "$tgt_logfile"
+    # handle machine account password
+  *.macct)
+    url="--url=/var/lib/samba/private/sam.ldb"
+    LDBSEARCH="$(which ldbsearch) $url"
+    LDBMODIFY="$(which ldbmodify) $url"
+    imagemacct="$LINBODIR/${RSYNC_REQUEST##*/}"
+    # upload samba machine password hashes to host's ad machine account
+    if [ -s "$imagemacct" ]; then
+      echo "Machine account ldif file: $imagemacct"
+      echo "Host: $compname"
+      # get dn of host
+      dn="$($LDBSEARCH "(&(sAMAccountName=$compname$))" | grep ^dn | awk '{ print $2 }')"
+      if [ -n "$dn" ]; then
+        echo "DN: $dn"
+        ldif="/var/tmp/${compname}_macct.$$"
+        ldbopts="--nosync --verbose --controls=relax:0 --controls=local_oid:1.3.6.1.4.1.7165.4.3.7:0 --controls=local_oid:1.3.6.1.4.1.7165.4.3.12:0"
+        sed -e "s|@@dn@@|$dn|" "$imagemacct" > "$ldif"
+        $LDBMODIFY $ldbopts "$ldif"
+        rm -f "$ldif"
+      else
+        echo "Cannot determine DN of $compname! Aborting!"
+      fi
     fi
-    cat "$FILE" >> "$tgt_logfile"
-  fi
-  rm -f "$FILE"
-  touch "$FILE"
-  ;;
+    ;;
 
- # provide host's opsi key for download
- *.opsikey)
-  # invoked by linbo_cmd on postsync
-  # if opsi server is configured and host is opsimanaged
-  if ([ -n "$opsiip" ] && opsimanaged "$compname"); then
-   echo "Opsi key file $(basename $FILE) requested."
-   key="$(grep ^"$RSYNC_HOST_NAME" "$LINBOOPSIKEYS" | awk -F\: '{ print $2 }')"
-   if [ -n "$key" ]; then
-    echo "Opsi key for $RSYNC_HOST_NAME found, providing key file."
-    echo "$key" > "$FILE"
-    chmod 644 "$FILE"
-    # upload opsiip to client
-    linbo-ssh "$RSYNC_HOST_ADDR" "echo $opsiip > /tmp/opsiip"
-    # get opsi server cert and provide it to client
-    opsipem="opsiconfd.pem"
-    rsync -v "$opsiip:/etc/opsi/$opsipem" "$LINBODIR/$opsipem"
-    chmod 600 "$LINBODIR/$opsipem"
-    linbo-scp -v "$LINBODIR/$opsipem" "$RSYNC_HOST_ADDR:/tmp"
-   fi
-  fi
- ;;
+    # fetch logfiles from client
+  *.log)
+    host_logfile="$(basename "$FILE")"
+    echo "Upload request for $host_logfile."
+    src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
+    tgt_logfile="$LINBOLOGDIR/$host_logfile"
+    linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
+    if [ -s "$FILE" ]; then
+      echo "## Log session begin: $(date) ##" >> "$tgt_logfile"
+      cat "$FILE" >> "$tgt_logfile"
+      echo "## Log session end: $(date) ##" >> "$tgt_logfile"
+    fi
+    rm -f "$FILE"
+    touch "$FILE"
+    ;;
 
- # patch image registry files with sambadomain if necessary
- *.reg)
-  search="Domain\"=\"$sambadomain\""
-  if ! grep -q "$search" "$FILE"; then
-    sed -i "s|Domain\"=.*|$search|g" "$FILE"
-  fi
- ;;
+    # fetch image status from client
+  *.status)
+    host_logfile="$(basename "$FILE")"
+    echo "Upload request for $host_logfile."
+    src_logfile="$(echo "$FILE" | sed -e "s|$LINBODIR/tmp/${compname}_|/tmp/|")"
+    tgt_logfile="$LINBOLOGDIR/$host_logfile"
+    linbo-scp -v "${RSYNC_HOST_ADDR}:$src_logfile" "$FILE" || RC="1"
+    if [ -s "$FILE" ]; then
+      if [ -s "$tgt_logfile" ]; then
+        # remove older entry for image
+        image="$(cat "$FILE" | awk '{ print $3 }')"
+        [ -n "$image" ] && sed -i "/$image/d" "$tgt_logfile"
+      fi
+      cat "$FILE" >> "$tgt_logfile"
+    fi
+    rm -f "$FILE"
+    touch "$FILE"
+    ;;
 
- # handle windows product key request
- *.winkey)
-  # get key from workstations and write it to temporary file
-  if [ -n "$compname" ]; then
-   winkey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $7 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
-   officekey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $6 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
-   [ -n "$winkey" ] && echo "winkey=$winkey" > "$FILE"
-   [ -n "$officekey" ] && echo "officekey=$officekey" >> "$FILE"
-  fi
- ;;
+    # provide host's opsi key for download
+  *.opsikey)
+    # invoked by linbo_cmd on postsync
+    # if opsi server is configured and host is opsimanaged
+    if ([ -n "$opsiip" ] && opsimanaged "$compname"); then
+      echo "Opsi key file $(basename $FILE) requested."
+      key="$(grep ^"$RSYNC_HOST_NAME" "$LINBOOPSIKEYS" | awk -F\: '{ print $2 }')"
+      if [ -n "$key" ]; then
+        echo "Opsi key for $RSYNC_HOST_NAME found, providing key file."
+        echo "$key" > "$FILE"
+        chmod 644 "$FILE"
+        # upload opsiip to client
+        linbo-ssh "$RSYNC_HOST_ADDR" "echo $opsiip > /tmp/opsiip"
+        # get opsi server cert and provide it to client
+        opsipem="opsiconfd.pem"
+        rsync -v "$opsiip:/etc/opsi/$opsipem" "$LINBODIR/$opsipem"
+        chmod 600 "$LINBODIR/$opsipem"
+        linbo-scp -v "$LINBODIR/$opsipem" "$RSYNC_HOST_ADDR:/tmp"
+      fi
+    fi
+    ;;
 
- # handle windows activation tokens archive
- winact-upload)
-  RC=0
-  FILE="${FILE%.upload}"
-  # fetch archive from client
-  echo "Upload request for windows activation tokens archive."
-  linbo-scp "${RSYNC_HOST_ADDR}:/cache/$(basename $FILE)" "${FILE}.tmp" || RC="1"
-  # if archive file already exists try to merge old and new archives
-  if [ -s "$FILE" -a "$RC" = "0" ]; then
-   echo "Updating existing archive $FILE."
-   tmpdir="/var/tmp/winact-upload.$$"
-   curdir="$(pwd)"
-   mkdir -p "$tmpdir"
-   # extract old archive to tmpdir
-   tar xf "$FILE" -C "$tmpdir" || RC="1"
-   if [ "$RC" = "0" ]; then
-    # extract uploaded archive over old archive in tmpdir
-    tar xf "${FILE}.tmp" -C "$tmpdir" || RC="1"
-   fi
-   if [ "$RC" = "0" ]; then
-    rm -f "${FILE}.tmp"
-    cd "$tmpdir"
-    # pack content of tmpdir to temporary archive
-    tar czf "${FILE}.tmp" * || RC="1"
-    cd "$curdir"
-   fi
-   rm -rf "$tmpdir"
-  fi
-  # move uploaded file in place
-  if [ "$RC" = "0" ]; then
-   rm -f "$FILE"
-   mv "${FILE}.tmp" "$FILE" || RC="1"
-  fi
-  if [ "$RC" = "0" ]; then
-   echo "Upload of $FILE successfully finished."
-  else
-   echo "Sorry. Upload of $FILE failed."
-  fi
-  rm -f "$PIDFILE"
- ;;
+    # patch image registry files with sambadomain if necessary
+  *.reg)
+    search="Domain\"=\"$sambadomain\""
+    if ! grep -q "$search" "$FILE"; then
+      sed -i "s|Domain\"=.*|$search|g" "$FILE"
+    fi
+    ;;
 
- # prepare download of local grub.cfg
- grub-local)
-  grubcfg_tpl="$LINBOTPLDIR/grub.cfg.local"
-  group="$(basename "$FILE" | awk -F\. '{ print $2 }')"
-  startconf="$LINBODIR/start.conf.$group"
-  append="$(linbo_kopts "$startconf") localboot"
-  sed -e "s|linux \$linbo_kernel .*|linux \$linbo_kernel $append|g" "$grubcfg_tpl" > "$FILE"
- ;;
+    # handle windows product key request
+  *.winkey)
+    # get key from workstations and write it to temporary file
+    if [ -n "$compname" ]; then
+      winkey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $7 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
+      officekey="$(grep ^[a-zA-Z0-9] $WIMPORTDATA | awk -F\; '{ print $2 " " $6 }' | grep -w $compname | awk '{ print $2 }' | tr a-z A-Z)"
+      [ -n "$winkey" ] && echo "winkey=$winkey" > "$FILE"
+      [ -n "$officekey" ] && echo "officekey=$officekey" >> "$FILE"
+    fi
+    ;;
 
- *) ;;
+    # handle windows activation tokens archive
+  winact-upload)
+    RC=0
+    FILE="${FILE%.upload}"
+    # fetch archive from client
+    echo "Upload request for windows activation tokens archive."
+    linbo-scp "${RSYNC_HOST_ADDR}:/cache/$(basename $FILE)" "${FILE}.tmp" || RC="1"
+    # if archive file already exists try to merge old and new archives
+    if [ -s "$FILE" -a "$RC" = "0" ]; then
+      echo "Updating existing archive $FILE."
+      tmpdir="/var/tmp/winact-upload.$$"
+      curdir="$(pwd)"
+      mkdir -p "$tmpdir"
+      # extract old archive to tmpdir
+      tar xf "$FILE" -C "$tmpdir" || RC="1"
+      if [ "$RC" = "0" ]; then
+        # extract uploaded archive over old archive in tmpdir
+        tar xf "${FILE}.tmp" -C "$tmpdir" || RC="1"
+      fi
+      if [ "$RC" = "0" ]; then
+        rm -f "${FILE}.tmp"
+        cd "$tmpdir"
+        # pack content of tmpdir to temporary archive
+        tar czf "${FILE}.tmp" * || RC="1"
+        cd "$curdir"
+      fi
+      rm -rf "$tmpdir"
+    fi
+    # move uploaded file in place
+    if [ "$RC" = "0" ]; then
+      rm -f "$FILE"
+      mv "${FILE}.tmp" "$FILE" || RC="1"
+    fi
+    if [ "$RC" = "0" ]; then
+      echo "Upload of $FILE successfully finished."
+    else
+      echo "Sorry. Upload of $FILE failed."
+    fi
+    rm -f "$PIDFILE"
+    ;;
+
+    # prepare download of local grub.cfg
+  grub-local)
+    grubcfg_tpl="$LINBOTPLDIR/grub.cfg.local"
+    group="$(basename "$FILE" | awk -F\. '{ print $2 }')"
+    startconf="$LINBODIR/start.conf.$group"
+    append="$(linbo_kopts "$startconf") localboot"
+    sed -e "s|linux \$linbo_kernel .*|linux \$linbo_kernel $append|g" "$grubcfg_tpl" > "$FILE"
+    ;;
+
+  *) ;;
 
 esac
 
